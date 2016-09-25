8e693bd043a7568f17a5477fac3f85c89cf43825
Grisha Bordyugov <gregbg@gmail.com>
2016-09-25 21:56:56 +0200

    dedicated class instead of pairs (score, path) in the alignment matrix

--- nw.hs
+++ nw.hs
@@ -1,8 +1,18 @@
 import Data.Array
 
+-- match, gap in the first sequence, gap in the second sequence
 data Alignment = Match | GapA | GapB deriving (Show, Eq, Ord)
 
-similarity a b
+-- a single cell in the score/path matrix
+data MCell = MC { score :: Int
+                , align :: Alignment
+                } deriving (Eq, Show)
+
+instance Ord MCell where
+  compare (MC s _) (MC t _) = compare s t -- don't compare alignment
+
+
+sim a b
   | a == b    = 1
   | otherwise = 0
 
@@ -13,16 +23,16 @@ nw x' y' = let b = bt lx ly in (reverse $ fst b, reverse $ snd b) where
   ( x,  y) = (listArray (1, lx) x', listArray (1, ly) y')
   a = listArray bounds [f i j | (i, j) <- range bounds]
   bounds = ((0, 0), (lx, ly)) 
-  f 0 j = (-j, GapA)
-  f i 0 = (-i, GapB)
-  f i j = maximum [ (fst (a ! (i-1, j-1)) + similarity (x!i) (y!j), Match)
-                  , (fst (a ! (i-1, j-0)) + gap, GapB)
-                  , (fst (a ! (i-0, j-1)) + gap, GapA) ]
+  f 0 j = MC (-j) GapA
+  f i 0 = MC (-i) GapB
+  f i j = maximum [ MC (score (a ! (i-1, j-1)) + sim (x!i) (y!j)) Match
+                  , MC (score (a ! (i-1, j-0)) + gap)             GapB
+                  , MC (score (a ! (i-0, j-1)) + gap)             GapA ]
   bt 0 0 = ("", "") -- backtracking 
   bt i j = case a ! (i, j) of
-    (_, Match) -> let (px, py) = bt (i-1) (j-1) in ((x!i):px, (y!j):py)
-    (_, GapA)  -> let (px, py) = bt (i-0) (j-1) in (  '-':px, (y!j):py)
-    (_, GapB)  -> let (px, py) = bt (i-1) (j-0) in ((x!i):px,   '-':py)
+    (MC _ Match) -> let (px, py) = bt (i-1) (j-1) in ((x!i):px, (y!j):py)
+    (MC _ GapA)  -> let (px, py) = bt (i-0) (j-1) in (  '-':px, (y!j):py)
+    (MC _ GapB)  -> let (px, py) = bt (i-1) (j-0) in ((x!i):px,   '-':py)
 
 -- compare insulin
 
